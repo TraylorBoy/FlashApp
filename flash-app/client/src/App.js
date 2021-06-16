498b602249dcccf4fef11434986e8916845602dd
TraylorBoy <traylorboy@protonmail.com>
2021-06-15 18:12:07 -0600

    updated ui

--- flash-app/client/src/App.js
+++ flash-app/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import getWeb3 from "./getWeb3";
 
-import { Flex, Box, Button } from 'rimble-ui';
+import { Flex, Box, Card, Button } from 'rimble-ui';
 import Navbar from './components/Navbar';
 
 import "./App.css";
@@ -15,7 +15,8 @@ class App extends Component {
 
 				web3: null,
 				accounts: null,
-				netId: 0
+				netId: 0,
+				isConnected: false
 
 			}
 
@@ -66,11 +67,20 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Navbar isConnected={false} netId={this.state.netId}/>
 
-				<Box p={3}>
-					<Button onClick={this.handleConnect}>Connect Wallet</Button>
-				</Box>
+				<Card>
+
+					<Navbar />
+
+					<Box p={3}>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultrices nibh nec fermentum bibendum. Nullam venenatis pellentesque nibh, vel ultrices dui consequat nec. Integer lectus massa, lacinia vitae odio at, porttitor mollis massa. Curabitur viverra eget magna vitae viverra. Cras leo tortor, sagittis ac massa non, aliquet imperdiet dui. Etiam rutrum eleifend orci ac iaculis. Fusce lacinia lobortis quam, quis semper libero cursus tincidunt. Nullam consequat, nisl vitae rutrum malesuada, lacus eros ornare lacus, nec fermentum orci nisl at mauris. Quisque in diam eu leo molestie pellentesque. Ut urna ex, mattis a dictum vitae, maximus nec purus. Integer finibus nulla odio, ut faucibus nibh ultrices et. Donec mattis odio metus, eget dictum nulla venenatis vitae. Aenean suscipit nisl diam, ac pharetra quam lobortis vitae. Aliquam id ex sit amet metus pulvinar molestie a vel erat. Suspendisse luctus turpis id sapien finibus rhoncus.</p>
+					</Box>
+
+					<Box p={3}>
+						<Button mainColor="black" size="small" onClick={this.handleConnect}>Connect Wallet</Button>
+					</Box>
+
+				</Card>
 
 			</div>
 		);
--- flash-app/client/src/components/Navbar.js
+++ flash-app/client/src/components/Navbar.js
@@ -1,23 +1,15 @@
 // Navigation bar at the top of the application
-// Displays text if user wallet is connected or not
+// Displays title
 import React from 'react';
 import { Flex, Box, Heading } from 'rimble-ui';
 
 function Navbar(props) {
 
   return (
-    <Flex alignItems="center" borderBottom="1px solid">
+    <Flex borderBottom="1px solid">
 
-      <Box p={3} width={ 1 / 4 }>
-        <p>Connected: { props.isConnected ? props.userAddress : "" }</p>
-      </Box>
-
-      <Box p={3} width={ 2 / 4 }>
+      <Box p={3} width={ 1 } >
         <Heading as="h1">FlashApp</Heading>
-      </Box>
-
-      <Box p={3} width={ 1 / 4 }>
-        <p>Network: { props.netId === 0 ? "" : props.netId }</p>
       </Box>
 
     </Flex>
