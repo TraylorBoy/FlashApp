a697b12677ab9d652c2db591becd5ad8f1bdff07
TraylorBoy <traylorboy@protonmail.com>
2021-06-21 10:25:39 -0600

    finished contract script

--- flash-app/server/core/contract.js
+++ flash-app/server/core/contract.js
@@ -1,7 +1,30 @@
+// FlashApp Contract
+// - Setup script intended to be used with the FlashApp API (flashappi)
+// author: Marques Traylor
 const Contract = require('web3-eth-contract');
 
 let props = {
   _provider: null,
   abi: null,
   address: "",
+}
+
+// Setup contract instance
+const setup = async (provider, abi, address) => {
+  console.log('Creating FlashApp contract');
+
+  // Will be passed to new Contract instance
+  props._provider = provider;
+  props.abi = abi;
+  props.address = address;
+
+  return props;
+}
+
+module.exports = FlashApp = async (provider, abi, address) => {
+  Contract.setProvider(provder);
+
+  const flashApp = new Contract(abi, address);
+
+  return flashApp;
 }
