1aa290ed37d5a905983a05e863443215d77afe89
TraylorBoy <traylorboy@protonmail.com>
2021-06-22 21:16:54 -0600

    ITX tank deposit tests passes

--- flash-app/test/flash_app.js
+++ flash-app/test/flash_app.js
@@ -1,22 +1,56 @@
+require("dotenv").config();
 const FlashApp = artifacts.require("FlashApp");
 const BN = require("bn.js");
 const assert = require("assert");
+const { ethers } = require("ethers");
 
-contract("FlashApp", (accounts) => {
+const INFURA_ITX = "0x015C7C7A7D65bbdb117C573007219107BD7486f9";
+const TEST_AMOUNT = "0.0001";
+
+contract("FlashApp", () => {
 
   let flashapp;
   let wallet;
+  let itx; // Infura ITX Provider https://infura.io/docs/transactions
 
   beforeEach("should setup the flashapp contract and test wallet account instances", async () => {
 
     flashapp = await FlashApp.deployed();
 
-    wallet = accounts[0];
+    itx = new ethers.providers.InfuraProvider(
+      "kovan", // Network
+      process.env.ID // Infura Project ID
+    )
+
+    wallet = new ethers.Wallet(process.env.KEY, itx); // Create wallet for transactions to be routed through ITX -> Kovan
 
   });
 
+  it("should check ITX balance > 0 of developer's account, if not then it transfers eth to the ITX account", async () => {
+    // Check initial balance
+    const startBalance = await itx.send("relay_getBalance", [wallet.address]);
 
-  it("should send premium from wallet to contract, perform flashloan then emit event LoanExecuted(reverse, amount, fee), pay back loan and emit event LoanCompleted(true/false)", async () => {
+    if (startBalance.balance <= 0) {
+      // Send amount to ITX account
+      const tx = await wallet.sendTransaction({
+        to: INFURA_ITX,
+        value: ethers.utils.parseUnits(TEST_AMOUNT, "ether")
+      });
+
+      // Waiting for transaction to be mined
+      await tx.wait();
+    }
+
+    // Check new balance
+    const currentBalance = await itx.send("relay_getBalance", [wallet.address]);
+    console.log("Current Balance: ", currentBalance.balance);
+
+    let isFunded = currentBalance.balance >= TEST_AMOUNT;
+    assert.equal(isFunded, true, "Transfer failed");
+  });
+
+
+  /*it("should send premium from wallet to contract, perform flashloan then emit event LoanExecuted(reverse, amount, fee), pay back loan and emit event LoanCompleted(true/false)", async () => {
 
     const token = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"; // Kovan Eth
 
@@ -24,6 +58,7 @@ contract("FlashApp", (accounts) => {
     const fee = new BN(0.09);
 
     const owed = web3.utils.toWei((amnt.mul(fee)).toString());
+    console.log(owed.toString());
 
     const initialBalance = await web3.eth.getBalance(wallet);
 
@@ -39,8 +74,6 @@ contract("FlashApp", (accounts) => {
       const newBalance = await web3.eth.getBalance(wallet);
 
       assert.equal(initialBalance >= newBalance, "Loan failed to complete.");
-      done();
-    })
-    .catch(err => done(err));
-  });
+    });
+  });*/
 });
